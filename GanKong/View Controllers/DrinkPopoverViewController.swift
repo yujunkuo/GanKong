4eaf7a1d7e84035ce414e06fee097d8d2d1b6d6e
pinya52 <a30515@gmail.com>
2020-04-16 22:37:54 +0800

    0416 drinks caculate calories

--- GanKong/View Controllers/DrinkPopoverViewController.swift
+++ GanKong/View Controllers/DrinkPopoverViewController.swift
@@ -12,13 +12,14 @@ class DrinkPopoverViewController: UIViewController, UIPickerViewDelegate, UIPick
     
     let fullScreenSize = UIScreen.main.bounds.size
     
-    // 茶每 700 毫升 100 大卡，奶類 700 毫升 390 大卡，果汁 100 毫升 50 大卡
-    // 咖啡 100 毫升 4.2 大卡
     let drinks = ["🍵(單茶)", "🥤(奶茶)", "☕️(咖啡)", "🍹(果汁)"]
     let drink_dict:Dictionary = ["🍵(單茶)" : "單茶", "🥤(奶茶)" : "奶茶",
                                  "☕️(咖啡)" : "咖啡", "🍹(果汁)" : "果汁"]
     let drink_volumne = ["咖啡小杯(240 c.c.)", "咖啡中杯(360 c.c.)", "咖啡大杯(480 c.c.)",  "小杯(500 c.c)", "大杯(700 c.c.)"]
-    let volumne_dict:Dictionary = ["咖啡小杯(240 c.c.)":240, "咖啡中杯(360 c.c.)":360, "咖啡大杯(480 c.c.)":480, "小杯(500 c.c)":500, "大杯(700 c.c.)":700]
+    let volumne_dict:Dictionary = ["咖啡小杯(240 c.c.)":240.0, "咖啡中杯(360 c.c.)":360.0, "咖啡大杯(480 c.c.)":480.0, "小杯(500 c.c)":500.0, "大杯(700 c.c.)":700.0]
+    // 茶每 700 毫升 100 大卡，奶類 700 毫升 390 大卡，果汁 100 毫升 50 大卡
+    // 咖啡 100 毫升 4.2 大卡
+    let calories_dict:Dictionary = ["單茶":0.142857, "奶茶":0.557142, "咖啡":0.042, "果汁":0.05]
     
     func hideKeyboard(tapG:UITapGestureRecognizer){
         self.view.endEditing(true)
@@ -56,23 +57,23 @@ class DrinkPopoverViewController: UIViewController, UIPickerViewDelegate, UIPick
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if component == 0 {
-            // 返回陣列 week 的成員數量
+            // 返回陣列 drinks 的成員數量
             return drinks.count
         }
         
         // 否則就是設置第二列
-        // 返回陣列 meals 的成員數量
+        // 返回陣列 drink_volumne 的成員數量
         return drink_volumne.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if component == 0 {
-            // 設置為陣列 week 的第 row 項資料
+            // 設置為陣列 drinks 的第 row 項資料
             return drinks[row]
         }
         
         // 否則就是設置第二列
-        // 設置為陣列 meals 的第 row 項資料
+        // 設置為陣列 drink_volumne 的第 row 項資料
         return drink_volumne[row]
     }
     
@@ -92,8 +93,12 @@ class DrinkPopoverViewController: UIViewController, UIPickerViewDelegate, UIPick
     
     @IBAction func doneButtonPressed(_ sender: Any){
         if(drinkSelectField.text != "" && volumneSelectField.text != ""){
-            print(drink_dict[String(drinkSelectField.text!)]!)
-            print(volumne_dict[String(volumneSelectField.text!)]!)
+            let drinkType = drink_dict[String(drinkSelectField.text!)]!
+            let volumne = volumne_dict[String(volumneSelectField.text!)]!
+            let calories:Double = calories_dict[drinkType]! * volumne
+            print(drinkType, volumne)
+            print("共攝取" + String(ceil(calories)) + "大卡")
+            
             self.dismiss(animated: true, completion: nil)
         }
         else if(drinkSelectField.text == ""){
